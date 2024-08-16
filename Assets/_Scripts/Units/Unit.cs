c86feff4ee7ff832ddb3cf6b749a5b0186aee78d
JAVHM <javanhmalpro@gmail.com>
2024-08-15 20:56:32 -0500

    unity layout upd

--- Assets/_Scripts/Units/Unit.cs
+++ Assets/_Scripts/Units/Unit.cs
@@ -9,6 +9,7 @@ namespace Pathfinding._Scripts.Units {
         public bool _isNpc =  true;
         public int _movements;
         public int _team;
+        public LayerMask _canWalkLayerMask;
 
         public void Init(Sprite sprite) {
             _renderer.sprite = sprite;
