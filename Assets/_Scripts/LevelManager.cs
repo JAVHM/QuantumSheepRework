8a5fd317c1e96ed5cc269d532a5fd957acbb0ad4
JAVHM <javanhmalpro@gmail.com>
2024-10-08 23:14:56 -0500

    DayCycle implemented

--- Assets/_Scripts/LevelManager.cs
+++ Assets/_Scripts/LevelManager.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum DayCycle
+{
+    Day,
+    Night,
+}
+
 public class LevelManager : MonoBehaviour
 {
-    public CardSO _selectedCardSO;
-    public int _dayCicle;
-    public int _dayCicleCurrent;
+    public DayCycle _currentCycle;
+    public int _dayCicleTime;
+    public int _dayCicleCurrentTime;
     public static LevelManager _instance;
+    public LevelUI _levelUI;
+    private LevelUI _instanceLevelUI;
 
     private void Awake()
     {
@@ -16,21 +24,46 @@ public class LevelManager : MonoBehaviour
 
     private void Start()
     {
-        GameplayManager.onMouseDown += SetCard;
+        _dayCicleCurrentTime = _dayCicleTime;
+        _instanceLevelUI = Instantiate(_levelUI);
+        if (_dayCicleTime != 0)
+            _instanceLevelUI.SetTimeText(_dayCicleCurrentTime);
+        else
+            _instanceLevelUI.SetTimeText();
+        _instanceLevelUI.SetGameIlumination(_currentCycle);
+        GameplayManager.onUnitMove += UpdateDaycicle;
     }
 
     public void OnDestroy()
     {
-        GameplayManager.onMouseDown -= SetCard;
+        GameplayManager.onUnitMove -= UpdateDaycicle;
     }
 
     public void UpdateDaycicle()
     {
-        
-    }
-
-    public void SetCard(CardSO newCard)
-    {
-        _selectedCardSO = newCard;
+        if (_dayCicleTime != 0)
+        {
+            _dayCicleCurrentTime--;
+            _instanceLevelUI.SetTimeText(_dayCicleCurrentTime);
+            if (_dayCicleCurrentTime <= 0)
+            {
+                _dayCicleCurrentTime = _dayCicleTime;
+                _instanceLevelUI.SetTimeText(_dayCicleCurrentTime);
+                if (_currentCycle == DayCycle.Day)
+                {
+                    _currentCycle = DayCycle.Night;
+                    _instanceLevelUI.SetGameIlumination(_currentCycle);
+                }
+                else
+                {
+                    _currentCycle = DayCycle.Day;
+                    _instanceLevelUI.SetGameIlumination(_currentCycle);
+                }
+            }
+        }
+        else
+        {
+            _instanceLevelUI.SetTimeText();
+        }
     }
 }
--- Assets/_Scripts/LevelUI.cs
+++ Assets/_Scripts/LevelUI.cs
@@ -1,9 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class LevelUI : MonoBehaviour
 {
     public TextMeshProUGUI _timeText;
+    public GameObject _gameIlumination;
+
+    public void SetTimeText(int time)
+    {
+        print("SetTimeText" + time);
+        _timeText.text = time.ToString();
+    }
+
+    public void SetTimeText()
+    {
+        _timeText.gameObject.SetActive(false);
+    }
+
+    public void SetGameIlumination(DayCycle part)
+    {
+        if (part == DayCycle.Day)
+        {
+            _gameIlumination.SetActive(false);
+        }
+        else
+        {
+            _gameIlumination.SetActive(true);
+        }  
+    }
 }
